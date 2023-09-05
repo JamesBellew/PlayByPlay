4f7f13eada99027695768331a5ec7094c39e8309
James Bellew <11jamesbellew@gmail.com>
2023-09-05 17:25:06 +0100

    started the board tempalte and added tailwindcss

--- PlayByPlay/src/App.jsx
+++ PlayByPlay/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="grid grid-cols-12 gap-4 h-screen bg-green-600">
+        <div class="col-start-3 col-span-8 bg-indigo-400">01</div>
+
+        <div class="bg-indigo-400 col-span-3">01</div>
+        <div class="bg-indigo-400 col-span-3">01</div>
+        <div class="bg-indigo-400 col-span-3">01</div>
+        <div class="bg-indigo-400 col-span-3">01</div>
+
+        <div class="bg-indigo-400 col-span-4">01</div>
+        <div class="bg-indigo-400 col-span-4">01</div>
+        <div class="bg-indigo-400 col-span-4">01</div>
+
+        <div class="bg-indigo-400 col-span-6">01</div>
+        <div class="bg-indigo-400 col-span-6">01</div>
+
+        <div class="col-start-3 col-span-8 bg-indigo-400">01</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
