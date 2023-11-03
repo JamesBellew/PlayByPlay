1e697c12c71978a1ae632fbb31275e2c1a98809f
James Bellew <11jamesbellew@gmail.com>
2023-11-03 23:51:40 +0000

    changes

--- PlayByPlay/src/Football/FootballComponents/AccountSidebar.jsx
+++ PlayByPlay/src/Football/FootballComponents/AccountSidebar.jsx
@@ -1,13 +1,12 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
 
 const AccountSideBar = (props) => {
-
   const [plays, setPlays] = useState([]);
-const [playCount,setPlayCount] = useState(plays.length)
+  const [playCount, setPlayCount] = useState(plays.length);
   useEffect(() => {
     // Fetch the plays data from local storage when the component mounts
-    const storedPlays = JSON.parse(localStorage.getItem('myData'));
+    const storedPlays = JSON.parse(localStorage.getItem("myData"));
     if (storedPlays && Array.isArray(storedPlays)) {
       setPlays(storedPlays);
     }
@@ -16,81 +15,89 @@ const [playCount,setPlayCount] = useState(plays.length)
 
   return (
     <>
+      <div className="my-auto w-full h-auto rounded-md text-white align-text-bottom items-end relative p-2">
+        <div class="bg-neutral-focus text-neutral-content align-bottom rounded-full w-14 mx-auto p-2">
+          <span class="text-3xl text-secondary">G</span>
+        </div>
+        <h1 className="m-3">Guest</h1>
+        <div className="btn-group">
+          <button className="btn">Sign In</button>
+          <button className="btn">Sign Up</button>
+        </div>
+        <div class="indicator mt-4">
+          {plays.length >= 1 && (
+            <span class="indicator-item badge badge-secondary"></span>
+          )}
+          <button
+            onClick={() => document.getElementById("my_modal_2").showModal()}
+            class="btn grid w-auto p-4 h-auto bg-base-300 place-items-center">
+            Saved Plays
+          </button>
+        </div>
 
-<div className="my-auto w-full h-auto rounded-md text-white align-text-bottom items-end relative p-2">
-<div class="bg-neutral-focus text-neutral-content align-bottom rounded-full w-14 mx-auto p-2">
-    <span class="text-3xl text-secondary">G</span>
-  </div>
-<h1 className="m-3">Guest</h1>
-<div className="btn-group">
-<button className="btn">Sign In</button>
-<button className="btn">Sign Up</button>
+        {/* Open the modal using document.getElementById('ID').showModal() method */}
+        <dialog id="my_modal_2" className="modal w-auto">
+          <div className="modal-box w-auto p-10">
+            <h3 className="font-bold text-lg mb-2">Saved Plays This Machine</h3>
+            <div className="overflow-x-auto w-auto">
+              {plays.length === 0 ? (
+                <>
+                  <p className="mt-5">No Plays Saved to this machine</p>
 
-</div>
-<div class="indicator mt-4">
-  {plays.length>=1 &&
-    <span class="indicator-item badge badge-secondary"></span> 
+                  <small>
+                    SetPlays may be store on your machine as text files saved
+                    under
+                    <pre data-prefix=">" class="text-warning mt-5">
+                      <code>PlaybyPlay_PlayName.txt</code>
+                    </pre>
+                    {/* <input className="btn">Look for Play</input> */}
+                    <input
+                      type="file"
+                      class="file-input w-full max-w-xs  border-none bg-none mt-2"
+                    />
+                  </small>
+                </>
+              ) : (
+                <table className="table table-xs">
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Name</th>
+                      <th>Date Saved</th>
+                      <th>Moves</th>
+                    </tr>
+                  </thead>
 
-  }
-  <button onClick={()=>document.getElementById('my_modal_2').showModal()} class="btn grid w-auto p-4 h-auto bg-base-300 place-items-center">Saved Plays</button>
-  
-</div>
-
-{/* Open the modal using document.getElementById('ID').showModal() method */}
-<dialog id="my_modal_2" className="modal w-auto">
-  <div className="modal-box w-auto p-10">
-    <h3 className="font-bold text-lg mb-2">Saved Plays This Machine</h3>
-    <div className="overflow-x-auto w-auto">
-    {plays.length ===0 ?
-    <>
-     <p className="mt-5">No Plays Saved to this machine</p>
-     
-     <small>SetPlays may be store on your machine as text files saved under 
-     <pre data-prefix=">" class="text-warning mt-5"><code>PlaybyPlay_PlayName.txt</code></pre> 
-     {/* <input className="btn">Look for Play</input> */}
-     <input type="file" class="file-input w-full max-w-xs  border-none bg-none mt-2" /> 
-     </small>
-    </>
-     :
-    
-    
-    
-    
-  <table className="table table-xs">
-    <thead>
-      <tr>
-        <th></th> 
-        <th>Name</th> 
-        <th>Date Saved</th> 
-        <th>Moves</th> 
-       
-  
-      </tr>
-    </thead> 
-    
-    <tbody>
-        {plays.map((play, index) => (
-          <tr key={index} className="cursor-pointer hover:bg-primary rounded-xl transition-all">
-            <th>{index + 1}</th>
-            <td>{play.name}</td>
-            <td>{play.date}</td>
-            <td>{play.firstArray.length}</td>
-          </tr>
-        ))}
-      </tbody>
-    
-  </table>
-}
-</div>
-    <div className="modal-action">
-      <form method="dialog">
-        {/* if there is a button in form, it will close the modal */}
-        <button className="btn">Close</button>
-      </form>
-    </div>
-  </div>
-</dialog>
-</div>
+                  <tbody>
+                    {plays && plays.length > 0 ? (
+                      plays.map((play, index) => (
+                        <tr
+                          key={index}
+                          className="cursor-pointer hover:bg-primary rounded-xl transition-all">
+                          <th>{index + 1}</th>
+                          <td>{play?.name ?? "N/A"}</td>
+                          <td>{play?.date ?? "N/A"}</td>
+                          <td>{play?.firstArray?.length ?? 0}</td>
+                        </tr>
+                      ))
+                    ) : (
+                      <tr>
+                        <td colSpan="4">No plays available</td>
+                      </tr>
+                    )}
+                  </tbody>
+                </table>
+              )}
+            </div>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+      </div>
     </>
   );
 };
--- PlayByPlay/src/Football/ViewPlay.jsx
+++ PlayByPlay/src/Football/ViewPlay.jsx
@@ -2,26 +2,209 @@ import React, { useState, useEffect, useRef } from "react";
 import Timeline from "./FootballComponents/Timeline";
 import SaveSetPlay from "./FootballComponents/SaveSetPlay";
 import AccountSideBar from "./FootballComponents/AccountSidebar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faPause, faForward } from "@fortawesome/free-solid-svg-icons";
 
-
-const ViewPlay = (props)=>{
-    return(
+const ViewPlay = (props) => {
+  const [Moves, setMoves] = useState([]);
+  const [showTimelineState, setshowTimelineState] = useState(false);
+  const [startingFifteenEditingState, updatestartingFifteenEditingState] =
+    useState(true);
+  const [players, setPlayers] = useState([
+    { playerNumber: 1, pitchPosition: "gk-1" },
+    { playerNumber: 2, pitchPosition: "fb-3" },
+    { playerNumber: 3, pitchPosition: "fb-6" },
+    { playerNumber: 4, pitchPosition: "fb-9" },
+    { playerNumber: 5, pitchPosition: "hb-3" },
+    { playerNumber: 6, pitchPosition: "hb-6" },
+    { playerNumber: 7, pitchPosition: "hb-9" },
+    { playerNumber: 8, pitchPosition: "mf-5" },
+    { playerNumber: 9, pitchPosition: "mf-7" },
+    { playerNumber: 10, pitchPosition: "mf-hf-3" },
+    { playerNumber: 11, pitchPosition: "mf-hf-6" },
+    { playerNumber: 12, pitchPosition: "mf-hf-9" },
+    { playerNumber: 13, pitchPosition: "ff-3" },
+    { playerNumber: 14, pitchPosition: "ff-6" },
+    { playerNumber: 15, pitchPosition: "ff-9" },
+  ]);
+  const [ballPosition, setBallPosition] = useState("gk-1");
+  const numDivs = 11;
+  const TenDivs = ({ outerDivName, count, forceHide = false }) => (
     <>
-    <div className="grid grid-cols-3 gap-1 mt-5 grid-rows-6  h-[90vh] top-[5vh] ">
-  <div className="bg-base-200 rounded-md "><p>This will be the information about the play</p></div>
-  <div className="bg-base-200 rounded-md col-span-2 ">This will be the controls of the play, Play,Pause,Timing etc....</div>
- 
-  <div className="col-span-3 bg-base-200 rounded-md row-span-5 ...">
+      {[...Array(count).keys()].map((index) => {
+        const divposition = outerDivName + "-" + (index + 1);
+        //getting the postion of the divs
 
-<p>This will be our pitch</p>
+        const positionIsUsed = players.some(
+          (player) => player.pitchPosition === divposition
+        );
+        const ballIsHere = divposition === ballPosition;
+        // console.log(ballIsHere);
+        const matchingPlayerNumber =
+          players.find((player) => player.pitchPosition === divposition)
+            ?.playerNumber || "Not Found";
+        const isPlayerMoving = Moves.some(
+          (move) => move.playerNumber === matchingPlayerNumber
+        );
+        const divRefs = useRef([]);
+        const shouldHide = false;
+        // = players.length === 14 && !positionIsUsed;
 
-  </div>
- 
- 
-</div>
-</>
-    )
-   
-}
+        return (
+          <div className="group my-auto">
+            <div
+              key={index}
+              ref={(el) => (divRefs.current[divposition] = el)}
+              className={`h-10 w-10 mx-auto my-auto text-black text-center cursor-auto
+                ${positionIsUsed ? "bg-white positionUsed " : "bg-primary/0"}
+                ${
+                  startingFifteenEditingState
+                    ? "  transition-all bg-primary"
+                    : "bg-slate-400 opacity-20 disabled"
+                }
+                ${
+                  isPlayerMoving
+                    ? "  bg-orange-200   border-2 border-blue-500"
+                    : ""
+                }
+                ${shouldHide ? "opacity-0 cursor-default" : ""}
+              
+                p-2 m-2 rounded-full`}>
+              <span
+                className={` ${
+                  positionIsUsed ? "text-black" : "text-white"
+                } duration-75`}>
+                {positionIsUsed ? matchingPlayerNumber : ""}
+                {ballIsHere && showTimelineState && ballEditingState && (
+                  <div className="football absolute z-100 cursor-pointer hover:bg-primary bg-black h-4 w-4 mx-auto ml-1 mt-2 rounded-full"></div>
+                )}
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-1 mt-5 grid-rows-6  h-[90vh] top-[5vh] ">
+        <div className="bg-base-200 rounded-md p-2 ">
+          {/* <h1 className="text-primary">Pistol Offence</h1>
+          <small className="text-xs">Created on 02/11/2023</small> */}
+          <div className="stat">
+            <div className="stat-figure text-primary">
+              <div className="avatar online">
+                <div className="w-14 rounded-full">
+                  <img src="https://images.pexels.com/photos/1722198/pexels-photo-1722198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+                </div>
+              </div>
+              <p>James</p>
+            </div>
+            <div className="stat-title">Offensive Play</div>
+            <div className=" text-2xl font-bold text-primary">Pisol</div>
+            <div className="stat-desc">02/11/2023</div>
+          </div>
+        </div>
+        <div className="bg-base-200 rounded-md flex items-center justify-center h-full">
+          <div className="btn-group btn-group-vertical lg:btn-group-horizontal">
+            <button className="btn btn-active">
+              {" "}
+              <FontAwesomeIcon icon={faPlay} />
+            </button>
+            <button className="btn">
+              <FontAwesomeIcon icon={faPause} />
+            </button>
+            <button className="btn">
+              <FontAwesomeIcon icon={faForward} />
+            </button>
+          </div>
+        </div>
+
+        <div className="bg-base-200 flex items-center justify-center">
+          <ul className="steps">
+            <li className="step step-primary">Move 1</li>
+            <li className="step step-primary">Move 2</li>
+            <li className="step">Move 3</li>
+          </ul>
+        </div>
+
+        <div className="col-span-3 bg-base-200 rounded-md row-span-5 ...">
+          <div
+            className="flex pitch flex-col
+           rounded bg-base-200 w-4/6 mx-auto p-5 relative self-center
+             h-[70vh]">
+            <div className="flex-grow flex flex-col">
+              <div
+                className={`flex-grow mx-auto   grid ${
+                  numDivs === 11 ? "grid-cols-1" : "grid-cols-1"
+                }`}>
+                <TenDivs outerDivName="gk" count={1} />
+              </div>
+              <div
+                className={`flex-grow  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="fb" count={numDivs} />
+              </div>
+
+              <div
+                className={`flex-grow  h-10  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="fb-hf" count={numDivs} />
+              </div>
+              <div
+                className={`flex-grow  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="hb" count={numDivs} />
+              </div>
+              <div
+                className={`flex-grow  h-10  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="hb-mf" count={numDivs} />
+              </div>
+            </div>
+            <div
+              className={`flex-grow   grid ${
+                numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+              }`}>
+              <TenDivs outerDivName="mf" count={numDivs} />
+            </div>
+
+            <div className="flex-grow flex flex-col">
+              <div
+                className={`flex-grow  h-10  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="mf-hf" count={numDivs} />
+              </div>
+              <div
+                className={`flex-grow  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="hf" count={numDivs} />
+              </div>
+              <div
+                className={`flex-grow  h-10  grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="hf-ff" count={numDivs} />
+              </div>
+              <div
+                className={`flex-grow   grid ${
+                  numDivs === 11 ? "grid-cols-11" : "grid-cols-3"
+                }`}>
+                <TenDivs outerDivName="ff" count={numDivs} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ViewPlay;
